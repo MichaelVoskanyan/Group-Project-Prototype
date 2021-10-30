4e7237ce5b82b76b4d33f50d5a993162fabc8d67
MichaelTokarev <michael.voskanyan@icloud.com>
2021-10-29 19:07:04 -0600

    Initial Form Build

---
+++ Search/wndSearch.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CS3280_Group_Project.Search {
+	/// <summary>
+	/// Interaction logic for wndSearch.xaml
+	/// </summary>
+	public partial class wndSearch : Window {
+		public wndSearch () {
+			InitializeComponent ();
+		}
+	}
+}
