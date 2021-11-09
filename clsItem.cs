1055346225c3e8284f19f8cda00bf6194e9dcfe0
DenverJensen <jensendenver@gmail.com>
2021-11-09 01:03:51 -0700

    order and item class created

--- clsItem.cs
+++ clsItem.cs
@@ -8,16 +8,34 @@ namespace CS3280_Group_Project
 {
     class clsItem
     {
+        /// <summary>
+        /// private item ID variable
+        /// </summary>
         private int itemID;
 
+        /// <summary>
+        /// private name variable
+        /// </summary>
         private string name;
 
+        /// <summary>
+        /// private price variable
+        /// </summary>
         private decimal price;
 
+        /// <summary>
+        /// public item ID with get and set
+        /// </summary>
         public int ItemID { get => itemID; set => itemID = value; }
 
+        /// <summary>
+        /// public name with get and set
+        /// </summary>
         public string Name { get => name; set => name = value; }
 
+        /// <summary>
+        /// public price with get and set
+        /// </summary>
         public decimal Price { get => price; set => price = value; }
     }
 }
---
+++ clsOrder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3280_Group_Project
+{
+    class clsOrder
+    {
+        /// <summary>
+        /// private order ID
+        /// </summary>
+        private int orderID;
+
+        /// <summary>
+        /// private order date
+        /// </summary>
+        private DateTime orderdate;
+
+        /// <summary>
+        /// private order total
+        /// </summary>
+        private decimal orderTotal;
+
+        /// <summary>
+        /// private list of items ordered
+        /// </summary>
+        private List<clsItem> items;
+
+        /// <summary>
+        /// public order ID with get and set
+        /// </summary>
+        public int OrderID { get => orderID; set => orderID = value; }
+
+        /// <summary>
+        /// public order date with get and set
+        /// </summary>
+        public DateTime OrderDate { get => orderdate; set => orderdate = value; }
+
+        /// <summary>
+        /// public order total with get and set
+        /// </summary>
+        public decimal OrderTotal { get => orderTotal ; set => orderTotal = value; }
+
+        /// <summary>
+        /// public list of items ordered with get and set
+        /// </summary>
+        public List<clsItem> Items { get => items; set => items = value; }
+    }
+}
