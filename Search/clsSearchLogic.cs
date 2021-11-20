d0b9df98dd4366bbefe2bbf0d88c60130b54e574
RobertPaupard <robertpaupard@mail.weber.edu>
2021-11-20 14:18:38 -0700

    Search window 1st build

--- Search/clsSearchLogic.cs
+++ Search/clsSearchLogic.cs
@@ -1,10 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CS3280_Group_Project {
-	class clsSearchLogic {
+	class clsSearchLogic 
+	{
+		public static List<clsOrder> GetOrders()
+        {
+			try
+			{
+				DataSet dsOrders = clsSearchSQL.GetOrders();
+
+				List<clsOrder> orderList = new List<clsOrder>();
+
+				for (int i = 0; i < dsOrders.Tables[0].Rows.Count; i++)
+				{
+					clsOrder newOrder = new clsOrder(int.Parse(dsOrders.Tables[0].Rows[i][0].ToString()),
+							DateTime.Parse(dsOrders.Tables[0].Rows[i][1].ToString()),
+							decimal.Parse(dsOrders.Tables[0].Rows[i].ItemArray[2].ToString()));
+
+					orderList.Add(newOrder);
+				}
+
+				return orderList;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+							MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+			}
+        }
+
+		public static List<int> GetInvoiceNumbers()
+        {
+			try
+			{
+				DataSet ds = clsSearchSQL.GetInvoiceNumbers();
+
+				List<int> invoiceNumberList = new List<int>();
+
+				for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+				{
+					//check  this code here to see if it's correct
+					int ID = int.Parse(ds.Tables[0].Rows[i][0].ToString());
+
+					invoiceNumberList.Add(ID);
+				}
+			
+				return invoiceNumberList;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+							MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+			}
+        }
+
+		public static List<DateTime> GetInvoiceDates()
+        {
+			DataSet ds = clsSearchSQL.GetInvoiceDates();
+
+			List<DateTime> invoiceDateList = new List<DateTime>();
+
+			for(int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+				//check  this code here to see if it's correct
+				DateTime date = DateTime.Parse(ds.Tables[0].Rows[i][1].ToString());
+
+				invoiceDateList.Add(date);
+			}
+
+			return invoiceDateList;
+        }
+
+		public static List<decimal> GetTotalCharges()
+        {
+			try
+			{
+				DataSet ds = clsSearchSQL.GetTotalCharges();
+
+				List<decimal> totalChargesList = new List<decimal>();
+
+				for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+				{
+					//check  this code here to see if it's correct
+					decimal charge = decimal.Parse(ds.Tables[0].Rows[i].ItemArray[2].ToString());
+
+					totalChargesList.Add(charge);
+				}
+
+				return totalChargesList;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+							MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+			}
+		}
 	}
 }
--- Search/wndSearch.xaml.cs
+++ Search/wndSearch.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -19,26 +22,273 @@ namespace CS3280_Group_Project {
 	public partial class wndSearch : Window {
 
 		public MainWindow MainWindow;
+		/// <summary>
+		/// int ID of order selected
+		/// </summary>
+		private int ID;
 
-		public wndSearch () {
+		public wndSearch() 
+		{
 			InitializeComponent ();
 			WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+
+			loadOrdersDataGrid();
+
+			loadInvoiceNumberComboBox();
+
+			loadInvoiceDateComboBox();
+
+			loadTotalChargeComboBox();
+
+		}
+
+		private void loadInvoiceNumberComboBox()
+        {
+			try
+			{
+				//needs to load cboInvoiceNumber
+				//trying to pass a list of type int for the invoice numbers from the database
+				List<int> invoiceNumberList = clsSearchLogic.GetInvoiceNumbers();
+				var bindingList = new BindingList<int>(invoiceNumberList);
+				var source = new BindingSource(bindingList, null);
+				cboInvoiceNumber.ItemsSource = source;
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}
+		}
+
+		private void loadInvoiceDateComboBox()
+        {
+			try
+			{
+				//needs to load cboInvoiceDate
+				List<DateTime> invoiceDateList = clsSearchLogic.GetInvoiceDates();
+				var bindingList = new BindingList<DateTime>(invoiceDateList);
+				var source = new BindingSource(bindingList, null);
+				cboInvoiceDate.ItemsSource = source;
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}
+
+		}
+
+		private void loadTotalChargeComboBox()
+        {
+			try
+			{
+				//needs to load cboTotalCharge
+				List<decimal> totalChargesList = clsSearchLogic.GetTotalCharges();
+				var bindingList = new BindingList<decimal>(totalChargesList);
+				var source = new BindingSource(bindingList, null);
+				cboTotalCharge.ItemsSource = source;
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}
+		}
+
+		private void loadOrdersDataGrid()
+        {
+			try
+			{
+				List<clsOrder> orderlist = clsSearchLogic.GetOrders();
+				var bindingList = new BindingList<clsOrder>(orderlist);
+				var source = new BindingSource(bindingList, null);
+				searchOrderGrid.ItemsSource = source;
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}
 		}
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
-			MainWindow = new MainWindow();
-			this.Hide();
-			MainWindow.ShowDialog();
-
+			try
+			{
+				MainWindow = new MainWindow();
+				this.Hide();
+				MainWindow.ShowDialog();
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}
 		}
 
         private void btnSelect_Click(object sender, RoutedEventArgs e)
-        {
-			///hardcoded 2 to simulate orderID 2 was selected and loaded into main window form
-			MainWindow = new MainWindow(2);
-			this.Hide();
-			MainWindow.ShowDialog();
+		{
+			try
+			{
+				//Once an order is selected in the datagrid the orderID is set in the DataGrid_SelectionChanged() method
+				//and a new MainWindow is created with the ID being passed to the MainWindow constructor
+				MainWindow = new MainWindow(ID);
+				this.Hide();
+				MainWindow.ShowDialog();
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}			
 		}
-    }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+			try
+			{
+				//orderID is set when selection is made in datagrid
+				clsOrder order = (clsOrder)searchOrderGrid.SelectedItem;
+				ID = order.OrderID;
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}						
+		}
+
+        private void cboInvoiceNumber_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			try
+			{
+				try
+				{
+					
+				}
+				catch (Exception ex)
+				{
+					HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+								MethodInfo.GetCurrentMethod().Name, ex.Message);
+				}
+				finally
+				{
+					//This code always executes
+				}
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}
+		}
+
+        private void cboInvoiceDate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+			try
+			{
+				try
+				{
+					
+				}
+				catch (Exception ex)
+				{
+					HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+								MethodInfo.GetCurrentMethod().Name, ex.Message);
+				}
+				finally
+				{
+					//This code always executes
+				}
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}
+		}
+
+        private void cboTotalCharge_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+			try
+			{
+				
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}
+		}
+
+        private void btnClearSelection_Click(object sender, RoutedEventArgs e)
+        {
+			try
+			{
+				
+			}
+			catch (Exception ex)
+			{
+				HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+							MethodInfo.GetCurrentMethod().Name, ex.Message);
+			}
+			finally
+			{
+				//This code always executes
+			}
+		}
+
+		private void HandleError(string sClass, string sMethod, string sMessage)
+		{
+			try
+			{
+                System.Windows.MessageBox.Show(sClass + "." + sMethod + "->" + sMessage);
+			}
+			catch (Exception ex)
+			{
+				System.IO.File.AppendAllText("C:\\Error.txt", Environment.NewLine +
+								"HandleError Exception: " + ex.Message);
+			}
+		}
+	}
 }
